61e390a038a2d522acc85a890959ccb2adaa8fd4
AleksandreSh <111062220+AleksandreSh@users.noreply.github.com>
2023-03-02 12:34:03 +0200

    fix2

--- src/js/app.js
+++ src/js/app.js
@@ -25,16 +25,89 @@ export const swiper = new Swiper('.first-section__swiper', {
    // And if we need scrollbar
    scrollbar: {
       el: '.swiper-scrollbar',
-      dragSize: 37.22,
+      dragSize: 38,
+      scaleX: 1,
    },
+  
    slidesPerView: 'auto',
    spaceBetween: 0,
 });
 
+// jQuery(document).ready(function($){
+	
+// 	let autoPlayDelay = 1500;
+	
+// 	let options = {
+// 		init: true,
+// 		// Optional parameters
+// 		loop: false,
+	
+// 		autoplay: {
+// 			delay: autoPlayDelay
+// 		},
 
+// 		// If we need pagination
+// 		/*pagination: {
+// 			el: '.swiper-pagination',
+// 			type: 'progressbar'
+// 		},*/
 
-
-
+//     	// Navigation arrows
+//     	navigation: {
+// 			nextEl: '.swiper-button-next',
+// 			prevEl: '.swiper-button-prev',
+//     	},
+//   	};
+	
+// 	let mySwiper = new Swiper ('.swiper-container', options);
+	
+// 	let slidersCount = mySwiper.params.loop ? mySwiper.slides.length - 2 : mySwiper.slides.length;
+// 	let widthParts = 100 / slidersCount;
+	
+// 	$('.swiper-counter .total').html(slidersCount);
+// 	for(let i=0; i<slidersCount; i++){
+// 		$('.swiper-progress-bar .progress-sections').append('<span></span>');
+// 	}
+	
+// 	function initProgressBar(){
+// 		let calcProgress = (slidersCount-1) * (autoPlayDelay + mySwiper.params.speed);
+//         calcProgress += autoPlayDelay;
+// 		$('.swiper-progress-bar .progress').animate({
+// 			width: '100%'
+// 		}, calcProgress, 'linear');
+// 	}
+	
+// 	initProgressBar();
+	
+// 	mySwiper.on('slideChange', function () {
+// 		let progress = $('.swiper-progress-bar .progress');
+		
+// 		$('.swiper-counter .current').html(this.activeIndex + 1);
+		
+// 		if( 
+// 			( 
+// 				this.progress == -0 || (this.progress == 1 && this.params.loop) 
+// 			) && !progress.parent().is('.stopped')
+// 		){
+// 			progress.css('width', '0');
+// 			if(this.activeIndex == 0){
+//             	initProgressBar();
+//             }
+// 		}
+		
+// 		if(progress.parent().is('.stopped')){		   
+// 			progress.animate({
+// 				'width': Math.round(widthParts * (this.activeIndex + 1)) + '%'
+// 			}, this.params.speed, 'linear');
+// 		}
+// 	});
+	
+// 	mySwiper.on('touchMove', function () {
+// 		$('.swiper-progress-bar .progress').stop().parent().addClass('stopped');
+// 	});
+	
+	
+// });
 
 //!!Підключення файлу з бургер-меню
 //import "./modules/burger-menu.js";
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -124,16 +124,14 @@ export function burgerMenu() {
    if (iconMenu){
       const menuBoxList = document.querySelector('.menu__box-list');
       const headerZero = document.querySelector('.header__zero');
-      //const firstSection = document.querySelector('.first-section__swiper');
-      //!!  Прибрав після помилки на сторінці 'News.html'. Можливо додав помилково
+
       const menuBurgerIcon = document.querySelector('.menu__burger-icon');
       const menyHeading = document.querySelector('.menu__heading');
       iconMenu.addEventListener("click", function (e) {
          document.body.classList.toggle('_lock');
          menuBurgerIcon.classList.toggle('_active');
          headerZero.classList.toggle('_active');
-         //firstSection.classList.toggle('_active');
-          //!!  Прибрав після помилки на сторінці 'News.html'. Можливо додав помилково
+        
          menuBoxList.classList.toggle('_active');
          menyHeading.classList.toggle('_active');
       })
